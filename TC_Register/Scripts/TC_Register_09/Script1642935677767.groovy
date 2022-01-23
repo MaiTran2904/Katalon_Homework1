2eff1cc3c4a6975d187eacdb5f6ccc5a2071db30
Tran Thi Nhu Mai <Mai Mai@PNVL062.pn.com.vn>
2022-01-23 18:52:26 +0700

    add folder Katalon_Homework1

---
+++ TC_Register/Scripts/TC_Register_09/Script1642935677767.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl(URL)
+
+WebUI.setText(findTestObject('Page/txt_Fullname_09'), long_full_name)
+
+WebUI.setText(findTestObject('Page/txt_email_09'), email)
+
+WebUI.setText(findTestObject('Page/txt_PhoneNumber_09'), phone_number)
+
+WebUI.setText(findTestObject('Page/txt_password_09'), password)
+
+WebUI.setText(findTestObject('Page/txt_repassword_09'), repassword)
+
+WebUI.click(findTestObject('Page/btn_Register_09'))
+
+WebUI.closeBrowser()
+
